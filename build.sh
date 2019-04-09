7fd84482d75dea4b0de21b4c07cd172b29a35d30
caijun <caijun@atoml.com>
2019-04-09 09:01:52 +0800

    auto commit 2019-04-09:09:01:51

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ echo "golang build..."
 APP_SOURCE_DIR=$GOPATH/src/github.com/marmotcai/${APP_NAME}
 
 SOURCE_URL=http://git.atoml.com/caijun/${APP_NAME}
-GIT_URL=http://git.atoml.com/caijun/${APP_NAME}.git
+ENV GIT_URL=github.com/marmotcai/uploadagent
 
 if [ ! -d "${APP_SOURCE_DIR}" ]; then
   echo "git clone ${GIT_URL} to ${APP_SOURCE_DIR}" 
-  git clone ${GIT_URL} ${APP_SOURCE_DIR}
+  go get -v ${GIT_URL}
 fi
 
 cd ${APP_SOURCE_DIR}
